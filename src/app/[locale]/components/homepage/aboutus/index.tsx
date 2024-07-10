dd40b0935a74fca2f83302deb26d3529c92bb7ec
Jose Perez <jjose.quevedo@gmail.com>
2024-07-10 20:17:11 +0200

    Update some components

--- src/app/[locale]/components/homepage/aboutus/index.tsx
+++ src/app/[locale]/components/homepage/aboutus/index.tsx
@@ -1,22 +1,26 @@
+import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Image from "next/image";
+import Link from "next/link";
 
 export const AboutUs = () => {
     return (
-        <section className="bg-white dark:bg-gray-900 text-gray-800 bg-[#f1f5f9]">
+        <section className="text-gray-800 bg-[#f1f5f9]">
             <div className="container flex flex-col justify-center p-6 mx-auto sm:py-12 lg:py-24 lg:flex-row lg:justify-between">
-            <div className="flex items-center justify-center w-full mt-6 lg:mt-0 lg:w-1/2">
-                    {/* <Image src='/images/woman.svg' width={650} height={650} alt=""  className="object-contain h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128"/> */}
-                    <Image width={200} height={200} className="w-full h-full lg:max-w-3xl" src='/images/woman.svg' alt="" />
+                <div className="flex items-center justify-center w-full mt-6 lg:mt-0 lg:w-1/2">
+                    <Image width={200} height={200} className="w-full h-full lg:max-w-3xl" src='/images/woman.svg' alt="Technologist Woman" />
                 </div>
-                <div className="flex flex-col justify-center p-6 text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
+                <div className="flex flex-col justify-center p-6 text-center rounded-sm lg:text-left lg:w-1/2">
                     <h1 className="text-5xl font-bold leading-none sm:text-6xl">
-                        About <span className="dark:text-violet-600">Us</span>
+                        About <span className="text-violet-600">Us</span>
                     </h1>
-                    <p className="mt-6 mb-8 text-lg sm:mb-12">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ac neque ut risus facilisis pellentesque a ac dui. Sed commodo ultricies nunc, sit amet sodales erat malesuada et. Quisque malesuada eros in ipsum venenatis, eu condimentum erat interdum. Maecenas porttitor ligula vitae sollicitudin auctor. 
+                    <p className="my-6 text-lg">
+                        {`We are a team of skilled developers dedicated to providing professional and simplified website development services. Our name, Insify Tech, blends "insight" and "simplify," reflecting our mission to create innovative and user-friendly web solutions. With expertise in PHP and Next.js technologies, we deliver top-notch web development tailored to your needs. Let us help you bring your vision to life with efficiency and creativity.`}
                     </p>
                     <div className="flex flex-col space-y-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start">
-                        <a rel="noopener noreferrer" href="#" className="px-8 py-3 text-lg font-semibold border rounded dark:border-gray-800">More</a>
+                        <Link rel="noopener noreferrer" href="#" className="text-md text-gray-600 font-semibold">
+                            Read more <FontAwesomeIcon icon={faArrowRight} />
+                        </Link>
                     </div>
                 </div>
             </div>
--- src/app/[locale]/components/homepage/hero/index.tsx
+++ src/app/[locale]/components/homepage/hero/index.tsx
@@ -13,13 +13,14 @@ export const Hero = () => {
         <header className="bg-white dark:bg-gray-900">
             <div className="container px-6 py-16 mx-auto">
                 <div className="items-center lg:flex">
-                    <div className="w-full lg:w-1/2">
+                    <div className="w-full lg:w-1/2 px-4">
                         <div className="lg:max-w-xl">
-                            <h1 className="text-3xl font-semibold text-gray-800 dark:text-white lg:text-6xl">{t('Simplified Website Development Solutions')}</h1>
+                            <h1 className="text-3xl font-semibold text-gray-800 dark:text-white lg:text-6xl">{t('Welcome to Insify Tech!')}</h1>
+                            {/* <h1 className="text-3xl font-semibold text-gray-800 dark:text-white lg:text-6xl">{t('Simplified Website Development Solutions')}</h1> */}
 
                             <h3 className="mt-3 text-2xl text-gray-600 dark:text-gray-400">{t("Insify Tech combines insight with simplification to deliver professional website development services using PHP and NextJS. Our skilled developers create innovative and easy-to-manage websites that perfectly capture your brand's essence. Let us help you Insify your tech presence!")}</h3>
 
-                            <button className="w-full px-5 py-2 mt-6 text-md tracking-wider text-white font-semibold transition-colors duration-300 transform bg-emerald-600 rounded-lg lg:w-auto hover:bg-emerald-500 focus:outline-none focus:bg-emerald-500">{t("Let's talk")}</button>
+                            <button className="px-5 py-2 mt-6 text-md tracking-wider text-white font-semibold transition-colors duration-300 transform bg-emerald-600 rounded-lg w-auto hover:bg-emerald-500 focus:outline-none focus:bg-emerald-500">{t("Let's talk")}</button>
                         </div>
                     </div>
 
